f7572d56abea90381a44400a5418d8822a07e908
kitaroghope <38127265+kitaroghope@users.noreply.github.com>
2023-10-12 21:24:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
+const port = 3300;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
